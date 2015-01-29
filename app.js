2d8661b83f67541c0a6c4661faa7d934ce605b3b
Clownyfunny <barbarin.pierre05@gmail.com>
2015-01-29 14:24:53 +0100

    Add CRUD for reviews

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var reviews = require('./routes/review');
+var database = require('./database/index');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/review', reviews);
+app.use('/reviews', reviews);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/review.js
+++ routes/review.js
@@ -1,49 +1,81 @@
 var express = require('express');
+var Reviews = require ('../database/reviews')
 var router = express.Router();
 
-var reviews = [
-{
-	name: 'MacDo',
-	placeType: 'Fastfood',
-	stars: 3
-}, {
-	name: 'Quick',
-	placeType: 'Fastfood',
-	stars: 5
-}];
+router.route('/')
 
-router.get('/', function(req, res, next) {
-	res.send(reviews);
+.get(function (req, res) {
+	Reviews.find({}, function (err, reviews) {
+		if (err) {
+			res.status(500).send({'error': err});
+		} else {
+			res.send(reviews);
+		}
+	});
 });
 
 router.get('/:index', function(req, res, next){
-	res.send(reviews[req.params.index]);
+	Reviews.findOne({_id: req.params.index}, function (err, reviews) {
+		if (err) {
+			res.status(500).send({'error': err});
+		} else {
+			res.send(reviews);
+		}
+	});
 });
 
 router.post('/new', function(req, res, next){
-	var newReview = req.query;
-	reviews.push(newReview);
-	res.json({message: 'review added'});
+	if(!req.body.name || !req.body.placeType || !req.body.stars){
+		res.status(400).send({ 'error' : 'Paramètres manquants'});
+	}
+	else{
+		var review = {
+			name: req.body.name,
+			placeType : req.body.placeType,
+			stars: req.body.stars,
+		};
+		Reviews.create(review, function(err, review){
+			if(err){
+				res.status(500).send({'error' : err});
+			}
+			else{
+				res.status(201).send(review);
+			}
+		});
+	}	
+
 });
 
 router.put('/edit/:index', function(req, res){
-	if(reviews[req.params.index] == undefined ){
-		console.log('Error this is not found');
-	}
-	else{
-		reviews[req.params.index] = req.query;
-		res.json ({message: 'review updated'});
-	}
+	Reviews.findOneAndUpdate({_id: req.params.index}, req.body , function (err, reviews) {
+		if (err) {
+			res.status(404).send({'error': err});
+		} else {
+			res.send(reviews);
+		}
+	});
 });
 
 router.delete('/delete', function(req, res, next){
-	reviews = [];
-	res.json({message: 'all reviews deleted'});
+	Reviews.remove({}, function(err, resp){
+		if(err){
+			res.status(404).send({'error' : err});
+		}
+		else{
+			res.status(204);
+		}
+	});
 });
 
 router.delete('/delete/:index', function(req, res, next){
-	reviews.splice(req.params.index, 1);
-	res.json({message: 'review deleted'});
+	Reviews.remove({_id: req.params.index}, function(err, resp){
+		if(err){
+			res.status(404).send({'error' : err});
+		}
+		else{
+			res.status(204);
+		}
+	});
 });
 
 module.exports = router;
